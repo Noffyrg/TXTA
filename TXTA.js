c8c4443695b4fa41a74769c1ea5b7dcdde1691a4
thatOneRobotProgrammer <noffyrg@gmail.com>
2016-02-24 20:33:33 -0500

    TXTA.js
    Added position and inventory variables
    Added:
    north
    south
    east
    west
    inventory

---
+++ TXTA.js
@@ -0,0 +1,58 @@
+
+
+confirm("Made by Chrome Lava")
+
+var Yvalue = 0
+var Xvalue  = 0
+var slot1 = "null"
+var slot2 = "null"
+var slot3 = "null"
+var slot4 = "null"
+var slot5 = "null"
+var slot6 = "null"
+//Position and inventory variables
+	
+	console.log("")
+	
+	var command = prompt("")
+	
+		if(command === "north")
+			{
+			Yvalue + 1 === Yvalue
+			}
+
+		else if(command === "south")
+			{
+			Yvalue - 1 === Yvalue
+			}
+
+		else if(command === "east")
+			{
+			Xvalue + 1 === Xvalue
+			}
+	
+		else if(command === "west")
+			{
+			Xvalue - 1 === Xvalue
+			}
+
+		else if(command === "inventory")
+			{
+			console.log(slot1 + ";" + slot2 + ";" + slot3 + ";" + slot4 + ";" + slot5 + ";" + slot6 + ".")
+			confirm(slot1 + ";" + slot2 + ";" + slot3 + ";" + slot4 + ";" + slot5 + ";" + slot6 + ".")
+			}
+
+
+		console.log("(" + Xvalue + "," + Yvalue + ")")
+console.log(command)
+
+
+
+
+
+
+
+
+
+
+
